7523428b485268ed212fc00f2d08d8a697e34101
tillcn <lovepuyu@gmail.com>
2017-11-04 09:48:34 +0800

    Add files via upload

---
+++ bizhi.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+import requests,os
+from bs4 import BeautifulSoup
+import os
+
+def request(all_url):
+    headers={
+    'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.101 Safari/537.36',
+    'Connection':'keep-alive',
+    'Referer':'https://wall.alphacoders.com/by_resolution.php?w=1920&h=1080&lang=Chinese'
+    }
+    contnet = requests.get(all_url, headers = headers)
+    return contnet
+
+for url_num in range(2,4966):
+    url_all= 'https://wall.alphacoders.com/by_resolution.php?w=1920&h=1080&lang=Chinese&page=' + str(url_num)
+    url = request(url_all)
+    soup = BeautifulSoup(url.text, 'lxml').find('div',id="container_page").find_all('div', class_="thumb-container-big ")
+    home_url = 'https://wall.alphacoders.com/'
+    name_url_save = {}
+    url_down = []
+    isexists = os.path.exists(os.path.join('d:\\bizhi','第'+str(url_num)+'页')) #.exists方法判断name是否存在，存在返回True
+    if not isexists:
+        print('创建目录d:\\bizhi'+'第'+str(url_num)+'页')
+        os.makedirs(os.path.join('d:\\bizhi','第'+str(url_num)+'页'))
+    else:
+        print('d:\\bizhi '+'第'+str(url_num)+'页'+'目录已存在')
+    os.chdir('d:\\bizhi\\'+'第'+str(url_num)+'页')
+    num=0
+    for i in soup:
+        k = i.find('a')
+        bizhi_name = k['title']
+        bizhi_url = home_url + k['href']
+        down_html = request(bizhi_url)
+        down_soup = BeautifulSoup(down_html.text, 'lxml').find('img', class_="img-responsive")
+        photo_name = down_soup['alt']
+        photo_url = down_soup['src']
+        name_url_save[photo_name] = photo_url
+        url_down.append(photo_url)
+        img = request(photo_url)
+        m = photo_url[-4:]
+        num += 1
+        i1 = str(num)
+        with open('bizhi.txt', 'a+') as f_dict:
+            f_dict.write(photo_name +'\t' + photo_url+'\n')
+        print('保存第'+i1+'张')
+        with open(photo_name + m, 'ab') as f:
+            f.write(img.content)
+    
